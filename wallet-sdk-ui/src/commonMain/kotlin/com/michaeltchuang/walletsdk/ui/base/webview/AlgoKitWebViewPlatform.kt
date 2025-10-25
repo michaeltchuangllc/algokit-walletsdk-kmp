735de9d764514e06f17111d840f8e64b90e7c6d4
Michael T Chuang <github@michaeltchuang.com>
2025-10-25 16:12:31 -0700

    feat: add in new app icon

--- wallet-sdk-ui/src/commonMain/kotlin/com/michaeltchuang/walletsdk/ui/base/webview/AlgoKitWebViewPlatform.kt
+++ wallet-sdk-ui/src/commonMain/kotlin/com/michaeltchuang/walletsdk/ui/base/webview/AlgoKitWebViewPlatform.kt
@@ -4,10 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import com.final_class.webview_multiplatform_mobile.webview.WebViewPlatform
 import com.final_class.webview_multiplatform_mobile.webview.controller.rememberWebViewController
-import kotlinx.serialization.Serializable
-
-@Serializable
-data object WebViewPlatformScreenNavigation
 
 @Composable
 fun AlgoKitWebViewPlatformScreen(url: String) {
