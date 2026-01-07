4ca4c3235d138ccf84af8e804720b20bd0e8478f
mithilesh <mithilesh13cs22@gmail.com>
2026-01-08 01:41:35 +0530

    libjnidispatch.so file added xHDWalletLib

--- wallet-sdk-core/build.gradle.kts
+++ wallet-sdk-core/build.gradle.kts
@@ -89,7 +89,7 @@ kotlin {
             implementation(libs.biometric)
 
             // toml files don't support aar files yet
-           // implementation("net.java.dev.jna:jna:5.17.0@aar")
+          //  implementation("net.java.dev.jna:jna:5.17.0@aar")
             implementation(libs.xhdwalletapi)
             implementation(libs.kotlin.bip39)
             implementation(libs.bcprov.jdk18on)
