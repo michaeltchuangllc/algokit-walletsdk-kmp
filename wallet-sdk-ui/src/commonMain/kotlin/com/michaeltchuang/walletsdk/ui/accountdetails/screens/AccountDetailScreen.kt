d4efdd03bdaec242a87d3a7c5061911fa765ad86
Michael T Chuang <github@michaeltchuang.com>
2026-02-05 09:29:19 -0800

    refactor: update url for dispenser

--- wallet-sdk-ui/src/commonMain/kotlin/com/michaeltchuang/walletsdk/ui/accountdetails/screens/AccountDetailScreen.kt
+++ wallet-sdk-ui/src/commonMain/kotlin/com/michaeltchuang/walletsdk/ui/accountdetails/screens/AccountDetailScreen.kt
@@ -196,7 +196,7 @@ internal fun ScreenContent(
                         AccountDetailWebviewItem(
                             icon = Res.drawable.ic_algo_sign,
                             title = localizedStringResource(Res.string.dispenser_add_funds_to_your_account),
-                            url = "https://bank.testnet.algorand.network/?account=$address",
+                            url = "https://lora.algokit.io/testnet/fund?address=$address",
                         )
                     }
 
